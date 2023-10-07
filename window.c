623f3f5d77d2679dba890aa9ab5c5d2fc5534e91
Serkan DURSUN <55033030+Sdursun@users.noreply.github.com>
2023-10-07 22:33:03 +0300

    Update window.c

--- window.c
+++ window.c
@@ -1,10 +1,10 @@
-//vc -c99 -lauto test.c -o Test
+//vc -c99 -lauto filename.c filename
 #include <proto/exec.h>
 #include <proto/intuition.h>
 #include <proto/dos.h>
 #include <intuition/intuitionbase.h>
 #include <intuition/intuition.h>
- 
+
 int main()
 {
 	char  Title[] = "Hello World!";
@@ -14,17 +14,13 @@ int main()
                         WA_Title,    	(ULONG)&Title,
                         WA_Activate, 	1,
 			TAG_END
-                     };
- 
+                     }; 
 	struct Window * pWin = OpenWindowTagList( NULL, (struct TagItem *)&WinTags );
 	if ( !pWin )
 	{
 		return -1;
 	}
- 
 	Delay( 500 );
- 
 	CloseWindow( pWin );
- 
 	return 0;
 }
